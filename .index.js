25790fc8ac91d81267da4fc91e5641b774fccb3e
Kp <kpericles23@hotmail.com>
2023-02-10 20:11:02 -0500

    Initial commit

---
+++ .index.js
@@ -0,0 +1,12 @@
+const express = require(`express`)
+require(`dotenv`).config()
+
+const app = express()
+
+app.get(`/`, (req, res) => {
+    res.send(`Hello World!`)
+})
+
+const PORT = process.env.PORT || 8080
+
+app.listen(PORT, console.log (`listening on port ${PORT}`))
