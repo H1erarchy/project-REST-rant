44c07e89885d9143ff42023ccf7acb1cfe1f4959
Kp <kpericles23@hotmail.com>
2023-02-18 22:41:01 -0500

    new places

--- controllers/places.js
+++ controllers/places.js
@@ -1,4 +1,9 @@
 const router = require('express').Router()
+const places = require('../models/places.js')
+
+router.get('/', (req,res) => {
+  res.render ('places/index', { places })
+})
 
 router.get('/new', (req, res) => {
     res.render('places/new')
@@ -6,8 +11,20 @@ router.get('/new', (req, res) => {
 
 router.post('/', (req, res) => {
   console.log(req.body)
-  res.send('POST /places')
+  if (!req.body.pic) {
+    // Default image if one is not provided
+    req.body.pic = '/images/jay-wennington-N_Y88TWmGwA-unsplash.jpg'
+  }
+  if (!req.body.city) {
+    req.body.city = 'Anytown'
+  }
+  if (!req.body.state) {
+    req.body.state = 'USA'
+  }
+  places.push(req.body)
+  res.redirect('/places')
 })
+
 
 
 // GET /places
---
+++ models/places.js
@@ -0,0 +1,13 @@
+module.exports = [{
+    name: 'H-Thai-ML',
+    city: 'Seattle',
+    state: 'WA',
+    cuisines: 'Thai, Pan-Asian',
+    pic: '/images/ralph-ravi-kayden-_6KWBeyFPeE-unsplash.jpg'
+}, {
+    name: 'Coding Cat Cafe',
+    city: 'Phoenix',
+    state: 'AZ',
+    cuisines: 'Coffee, Bakery',
+    pic: '/images/bobby-mc-leod-_XsJnWZHQHA-unsplash.jpg'
+}]
