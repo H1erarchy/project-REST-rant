cb082bfa118521e3124d4ae8b16736abe8bb4f99
Kp <kpericles23@hotmail.com>
2023-02-18 21:24:49 -0500

    adding styles to rest

--- index.js
+++ index.js
@@ -2,17 +2,18 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 
-app.set ('view engine', 'jsx')
-app.engine ('jsx', require ('express-react-views').createEngine())
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public'))
 
-app.use('/places', require('./controllers/places'))
+app.use('/places', require ('./controllers/places'))
 
-app.get('*', (req, res) => {
-    res.render('error404')
+app.get('/', (req, res) => {
+  res.render('home')
 })
 
 app.get('*', (req, res) => {
-    res.status(404).send('<h1>404 Page</h1>')
+  res.send('error404')
 })
 
 app.listen(process.env.PORT)
