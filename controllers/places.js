7ecd9a02f93275b30a586f848156dfcb8974debe
Kp <kpericles23@hotmail.com>
2023-02-27 20:48:25 -0500

    added the edit form

--- controllers/places.js
+++ controllers/places.js
@@ -9,6 +9,20 @@ router.get('/new', (req, res) => {
     res.render('places/new')
 })
 
+router.get('/:id/edit', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+      res.render('error404')
+  }
+  else if (!places[id]) {
+      res.render('error404')
+  }
+  else {
+    res.render('places/edit', { place: places[id] })
+  }
+})
+
+
 router.get('/:id', (req, res) => {
   let id = Number(req.params.id)
   if (isNaN(id)) {
@@ -18,7 +32,7 @@ router.get('/:id', (req, res) => {
     res.render('error404')
   }
   else {
-    res.render('places/show', { place: places [id], id})
+    res.render('places/show', { place: places [id] })
   }
 })
 
