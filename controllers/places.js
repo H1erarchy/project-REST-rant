9b640b5e171e6bd92f8a8b35383ed22d6c2c3ca6
Kp <kpericles23@hotmail.com>
2023-03-01 17:57:54 -0500

    fix4

--- controllers/places.js
+++ controllers/places.js
@@ -1,113 +1,64 @@
 const router = require('express').Router()
-const places = require('../models/places.js')
+const db = require('../models')
 
-router.get('/', (req,res) => {
-  res.render ('places/index', { places })
+router.get('/', (req, res) => {
+    db.Place.find()
+    .then((places) => {
+      res.render('places/index', { places })
+    })
+    .catch(err => {
+      console.log(err) 
+      res.render('error404')
+    })
 })
+
+
+router.post('/', (req, res) => {
+  db.Place.create(req.body)
+  .then(() => {
+      res.redirect('/places')
+  })
+  .catch(err => {
+      console.log('err', err)
+      res.render('error404')
+  })
+})
+
 
 router.get('/new', (req, res) => {
-    res.render('places/new')
+  res.render('places/new')
 })
-
-router.get('/:id/edit', (req, res) => {
-  let id = Number(req.params.id)
-  if (isNaN(id)) {
-      res.render('error404')
-  }
-  else if (!places[id]) {
-      res.render('error404')
-  }
-  else {
-    res.render('places/edit', { place: places[id] })
-  }
-})
-
-router.put('/:id', (req, res) => {
-  let id = Number(req.params.id)
-  if (isNaN(id)) {
-      res.render('error404')
-  }
-  else if (!places[id]) {
-      res.render('error404')
-  }
-  else {
-      if (!req.body.pic) {
-          req.body.pic = "/images/luis-villasmil-jPpHpgWNCKs-unsplash.jpg"
-      }
-      if (!req.body.city) {
-          req.body.city = 'Anytown'
-      }
-      if (!req.body.state) {
-          req.body.state = 'USA'
-      }
-      places[id] = req.body
-      res.redirect(`/places/${id}`)
-  }
-})
-
-
-
 
 router.get('/:id', (req, res) => {
-  let id = Number(req.params.id)
-  if (isNaN(id)) {
-    res.render('error404')
-  }
-  else if (!places[id]) {
-    res.render('error404')
-  }
-  else {
-    res.render('places/show', { place: places[id], id })
-  }
+  db.Place.findById(req.params.id)
+  .then(place => {
+      res.render('places/show', { place })
+  })
+  .catch(err => {
+      console.log('err', err)
+      res.render('error404')
+  })
 })
 
 
-router.post('/', (req, res) => {  
-  if (!req.body.pic) {
-    req.body.pic = '/images/jay-wennington-N_Y88TWmGwA-unsplash.jpg'
-  }
-  if (!req.body.city) {
-    req.body.city = 'Anytown'
-  }
-  if (!req.body.state) {
-    req.body.state = 'USA'
-  }
-  places.push(req.body)
-  res.redirect('/places')
+router.put('/:id', (req, res) => {
+  res.send('PUT /places/:id stub')
 })
 
 router.delete('/:id', (req, res) => {
-  let id = Number(req.params.id)
-  if (isNaN(id)) {
-    res.render('error404')
-  }
-  else if (!places[id]) {
-    res.render('error404')
-  }
-  else {
-    places.splice (id, 1)
-    res.redirect ('/places')
-  }
+  res.send('DELETE /places/:id stub')
 })
 
+router.get('/:id/edit', (req, res) => {
+  res.send('GET edit form stub')
+})
 
+router.post('/:id/rant', (req, res) => {
+  res.send('GET /places/:id/rant stub')
+})
 
-router.get('/places', (req, res) => {
-  let places = [{
-    name: 'H-Thai-ML',
-    city: 'Seattle',
-    state: 'WA',
-    cuisines: 'Thai, Pan-Asian',
-    pic: '/images/ralph-ravi-kayden-_6KWBeyFPeE-unsplash.jpg'
-  }, {
-    name: 'Coding Cat Cafe',
-    city: 'Phoenix',
-    state: 'AZ',
-    cuisines: 'Coffee, Bakery',
-    pic: '/images/bobby-mc-leod-_XsJnWZHQHA-unsplash.jpg'
-  }]
-  
-  res.render('places/index', { places })
+router.delete('/:id/rant/:rantId', (req, res) => {
+    res.send('GET /places/:id/rant/:rantId stub')
 })
 
 module.exports = router
