528e6851582f1394569eb892f198811c4a2fc6c2
Kp <kpericles23@hotmail.com>
2023-03-01 22:47:42 -0500

    fix5

--- controllers/places.js
+++ controllers/places.js
@@ -11,8 +11,6 @@ router.get('/', (req, res) => {
       res.render('error404')
     })
 })
-
-
 router.post('/', (req, res) => {
   db.Place.create(req.body)
   .then(() => {
@@ -33,15 +31,15 @@ router.post('/', (req, res) => {
   }
   })
 })
-
-
 router.get('/new', (req, res) => {
   res.render('places/new')
 })
 
 router.get('/:id', (req, res) => {
   db.Place.findById(req.params.id)
+  .populate ('comments')
   .then(place => {
+      console.log (place.comments)
       res.render('places/show', { place })
   })
   .catch(err => {
@@ -49,7 +47,26 @@ router.get('/:id', (req, res) => {
       res.render('error404')
   })
 })
-
+router.post('/:id/comment', (req, res) => {
+  console.log(req.body)
+  db.Place.findById(req.params.id)
+  .then(place => {
+      db.Comment.create(req.body)
+      .then(comment => {
+          place.comments.push(comment.id)
+          place.save()
+          .then(() => {
+              res.redirect(`/places/${req.params.id}`)
+          })
+      })
+      .catch(err => {
+          res.render('error404')
+      })
+  })
+  .catch(err => {
+      res.render('error404')
+  })
+})
 
 router.put('/:id', (req, res) => {
   res.send('PUT /places/:id stub')
